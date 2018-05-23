98ffad07581ee8a90fe016c3cbb3467f09928f1a
Andrew Beyond <spam@lykorian.org.ru>
2018-05-23 13:14:29 +0300

    rename testnet nodes

--- apps/tpnode/src/chainsettings.erl
+++ apps/tpnode/src/chainsettings.erl
@@ -62,9 +62,8 @@ settings_to_ets(NewSettings) ->
 
 get(Key, Settings) ->
   get(Key, Settings, fun() ->
-               {Chain, _}=gen_server:call(blockchain, last_block_height, -1),
-               true=is_integer(Chain),
-               Chain
+               {Chain, _}=gen_server:call(blockchain, last_block_height, 500),
+               true=is_integer(Chain)
            end).
 
 
@@ -72,7 +71,7 @@ get(allowempty, Settings, GetChain) ->
   get(<<"allowempty">>, Settings, GetChain);
 
 get(patchsig, Settings, GetChain) ->
-  case settings:get([<<"current">>,chain,patchsig],Settings) of
+  case settings:get([<<"current">>,chain,<<"patchsig">>],Settings) of
     I when is_integer(I) ->
       I;
     _ ->
